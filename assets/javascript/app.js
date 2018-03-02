7e98dbddd78f3d2770a05135f3e91b51d6a3213c
Kathy Kennedy <nomamanomama@yahoo.com>
2018-03-02 17:49:36 -0600

    user add flower functionality added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,27 @@
 var topics = ["rose", "orchid", "petunia", "lilly", "daisy", "geranium", "tulip", "bluebonnet", "lilac", "violet"];
-
-function createButtons () {
-    for (var i = 0; i<topics.length; i++){
-        var newButton = $("<button>").text(topics[i]).addClass("btn btn-primary topic").attr("data-search", topics[i]);
-        $("#buttons").append(newButton);
-
-    }
-}
-
-createButtons();
 var buttonName;
 var queryURLpre = "https://api.giphy.com/v1/gifs/search?api_key=pzWkzRyZiQFVEaxvVwyOIxLpA3h7ip8w&q=";
 var queryURLpost = "&limit=10&offset=0&rating=G&lang=en";
 
-$(".topic").on("click", function () {
-    buttonName="flower+" + $(this).data("search");
- //   console.log(this.data.value;
-    console.log(buttonName);
-    $.ajax({
-        url: queryURLpre + buttonName + queryURLpost,
-        method: "GET"
-    }).then(function (response) {
-        $("#gifCards").empty();
-        createCards(response);
-    });
-});
+$(document).ready( function() { 
+
+
+function createButtons (clear) {
+    for (var i = 0; i<topics.length; i++){
+        var newButton = $("<button>").text(topics[i]).addClass("btn btn-primary topic").attr("data-search", topics[i]);
+        if(clear) {
+            $("#gifbuttons").empty();
+            clear = false;
+        }
+        $("#gifbuttons").append(newButton);
+
+    }
+}
 
 function createCards(giphyList) {
     console.log(giphyList);
     for (var i = 0; i<giphyList.data.length; i++){
-         var giphyItem = giphyList.data[i];
+        var giphyItem = giphyList.data[i];
         var gifCard = $("<div>").addClass("card gifCard");
         var gifImage = $("<img>").addClass("card-img-top giphyImage").attr({ "data-gifurl": giphyItem.images.fixed_height_small.url, "data-gifstill": giphyItem.images.fixed_height_small_still.url, "src": giphyItem.images.fixed_height_small_still.url, "alt":"Card image top"});
         var gifBody = $("<div>").addClass("card-body").text("Rating: " + giphyItem.rating.toUpperCase()).attr("height", "50px");
@@ -37,16 +29,43 @@ function createCards(giphyList) {
     }
 }
 
-$("#gifCards").on("click", ".giphyImage", function () {
-    console.log(this);
-    var source = $(this).attr("src");
-    var gifMotion = $(this).data("gifurl");
-    var gifStill = $(this).data("gifstill");
-    if(source === gifMotion)
-        source = gifStill;
-    else
-        source = gifMotion; 
-    $(this).attr("src",source);
+
+    
+    createButtons ();
+
+    $(document).on("click",".topic", function () {
+        buttonName="flower+" + $(this).data("search");
+        console.log(buttonName);
+        $.ajax({
+            url: queryURLpre + buttonName + queryURLpost,
+            method: "GET"
+        }).then(function (response) {
+            $("#gifCards").empty();
+            createCards(response);
+        });
+    });
+
+
+    $("#gifCards").on("click", ".giphyImage", function () {
+        console.log(this);
+        var source = $(this).attr("src");
+        var gifMotion = $(this).data("gifurl");
+        var gifStill = $(this).data("gifstill");
+        if(source === gifMotion)
+            source = gifStill;
+        else
+            source = gifMotion; 
+        $(this).attr("src",source);
+    });
+
+    $("#addFlowerButton").on("click", function (e) {
+        //e.preventDefault();
+        buttonName=$("#addFlower").val();
+        topics.push(buttonName);
+        createButtons(true);
+        
+    });
+
+
+
 });
-
-
